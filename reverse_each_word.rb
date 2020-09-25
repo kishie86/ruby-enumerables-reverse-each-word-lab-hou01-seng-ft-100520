998902dbada735d6c41c1f991f986764bb3eb876
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-25 22:01:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word (sentence)
-   new_array = []
-  sentence_array = sentence.split 
+  new array = []
+  sentence_array = sentence.split
 sentence_array.collect do |words|
   new_array << words.reverse
   end
